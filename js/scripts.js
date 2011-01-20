277253312e398c9c589230aa8a790c25becd66e1
Adriaan Pelzer <adriaan@dragon.pitproduksies.org>
2011-01-20 15:06:51 +0200

    added a browser check for ie 6 in the javascript file to fix offset problem in ie6

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,17 @@
 $(document).ready(function(){
+        alert($.browser.version);
+    if ($.browser.msie) {
+        var verArray = ($.browser.version).split('.');
+        if ((verArray[0]) == 6) {
+            offset = 640;
+        } 
+    }
     $('#paging').data ('position', 0);
 
     $('#page_left').click (function () {
         if ($('#paging').data ('position') > 0) {
             $('#cat_content').animate ({
-                left: '+=644'
+                left: '+=' + offset
             }, 500, function () {
                 $('#paging').data ('position', ($('#paging').data ('position') - 1));
             });
@@ -14,7 +21,7 @@ $(document).ready(function(){
     $('#page_right').click (function () {
         if ($('#paging').data ('position') < ($('.post_container').size() - 1)) {
             $('#cat_content').animate ({
-                left: '-=644'
+                left: '-=' + offset
             }, 500, function () {
                 $('#paging').data ('position', ($('#paging').data ('position') + 1));
             });
