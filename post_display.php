80464263d398af6a8f7bc14943c3951efc63c4f8
Adriaan Pelzer <adriaan@dragon.pitproduksies.org>
2011-01-24 17:23:20 +0200

    various changes - added load spinner and drop down box to voices page

--- post_display.php
+++ post_display.php
@@ -6,7 +6,17 @@ Template Name: Post Display
 <?php get_header() ?>
 
 <?php $num_posts = (wp_count_posts()); ?>
-
+<section id="voices_drop_down">
+<?php $top_cat = get_cat_ID('Artist Name');
+$voices = wp_list_categories('title_li=&child_of='. $top_cat .'&echo=0'); 
+$voices = str_replace('li', 'option', $voices); ?>
+    <select id="voices_select">
+        <option selected="selected">Please Choose a Voice Artist</option>
+<?php
+print_r ($voices);
+?>
+    </select>
+</section>
 <div id="cat_container" class="body">
 <?php /*get_sidebar ();*/ ?>
     <div id="cat_content" style="width:<?php echo (41.625*($num_posts->publish)); ?>em">
@@ -23,12 +33,13 @@ foreach($myposts as $post) {
 }
 ?>
     </div><!-- #content .hfeed -->
-    <div id="gradient_left"><img src="<?php bloginfo('template_url'); ?>/images/post_container_gradient_left.png" /></div>
-    <div id="gradient_right"><img src="<?php bloginfo('template_url'); ?>/images/post_container_gradient_right.png" /></div>
+    <nav id="paging">
+        <div id="gradient_left"><span id="page_left"><a><img class="pngfix" src="<?php bloginfo('template_url'); ?>/images/post_container_gradient_left_arrows_sprite.png" width="125" height="334" /></a></span></div>
+        <div id="gradient_right"><span id="page_right"><a><img class="pngfix" src="<?php bloginfo('template_url'); ?>/images/post_container_gradient_right_arrows_sprite.png" width="125" height="334" /></a></span></div>
+    </nav>
 </div><!-- #container -->
-<nav id="paging">
+<!--nav id="paging">
     <span id="page_left"><a>&laquo;</a></span><span id="page_right"><a>&raquo;</a></span>
-</nav>
+</nav-->
 
 <?php get_footer() ?>
-
