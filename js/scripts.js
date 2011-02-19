74795144595f4ab0a88b926122fef4e0fe8cd43b
Adriaan Pelzer <adriaan@wewillraakyou.com>
2011-02-19 14:34:24 +0000

    Load soundclouds in order with JS, and static if noscript

--- js/scripts.js
+++ js/scripts.js
@@ -83,6 +83,7 @@ $(document).ready(function(){
     });
 
     $(".post_container").each (function () {
-        $(this).find ('.inner_container').html ($(this).data ('content'));
+        var content = $.base64Decode($(this).data ('content'));
+        $(this).find ('.inner_container').html (content);
     });
 });
